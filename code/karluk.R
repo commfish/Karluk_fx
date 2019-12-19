7fa7850f6dd8c28778f5d697324151fc38714833
Power <sarah.power@alaska.gov>
2019-12-19 10:42:05 -0900

    Clean up and annotations

--- code/karluk.R
+++ code/karluk.R
@@ -1,26 +1,28 @@
 # notes ----
 # forecasting using timeseries
 # sarah.power@alaska.gov
-# 11/14/2018
+# 2019
 
 # load ----
-library(fpp2)
-library(ggplot2)
-library(tidyverse)
-library(RDS)
-library(here)
-options(scipen=999)
+if(!require("tidyverse"))   install.packages("tidyverse") # data manipulation & graphing
+if(!require("RDS"))   install.packages("RDS") # forecasting
+if(!require("fpp2"))   install.packages("fpp2") # forecasting
+options(scipen=999) # prevent scientific notation everywhere
 
 # data ----
 
-karluk2018 <- read.csv('data/karluksmoothing.csv') 
-karluk <- as.ts(karluk2018)
 karluk2018 <- read.csv('data/karluk.csv') 
+# run = return to a river as determined by harvest and escapement estimates
+# ker = karluk early run
+# klr = karluk late run
+# ktr = karlut total run = early + late
+
+# create timeseries object
 ker <- as.ts(karluk2018$ker)
 klr <- as.ts(karluk2018$klr)
 ktr <- as.ts(karluk2018$ktr)
 
-#choose one
+#choose one set of data to do analysis on:
 karluk <- ker
 karluk <- klr
 karluk <- ktr
@@ -29,10 +31,16 @@ karluk <- ktr
 autoplot(karluk)
 ggAcf(karluk)
 ggAcf(diff(karluk))
-fc <- fch <- holt(y = karluk, h = 5,  exponential = TRUE) 
-fc <- fcn <- naive(karluk, h = 5)
-fc <- fcses <- ses(karluk, h = 5)
+
+# choose one type of timeseries forecast to run
+fc <- fch <- holt(y = karluk, h = 1,  exponential = TRUE) 
+fc <- fcn <- naive(karluk, h = 1)
+fc <- fcses <- ses(karluk, h = 1)
+
+# for checking and comparison
 summary(fc)
 checkresiduals(fc)
-#accuracy(fc)
+accuracy(fc)
+
+#plot ----
 autoplot(fc) + autolayer(fitted(fc))
